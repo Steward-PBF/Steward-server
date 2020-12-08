a2e09ea25e0178ed74308ba9a57589c14acc5039
Norbert Dziwinski <norbert.dziwinski@mayeryn.com>
2020-12-08 19:20:43 +0100

    initial commit

---
+++ src/main/java/pl/steward/Stewardserver/StewardServerApplication.java
@@ -0,0 +1,13 @@
+package pl.steward.Stewardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StewardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StewardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/steward/Stewardserver/StewardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.steward.Stewardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StewardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
